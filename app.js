1f7b00b1d23ec11a45cd34980aef54765752301f
Melih Kaçaman <mkacamann@gmail.com>
2020-05-15 14:48:10 +0300

    the last commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,9 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  console.log(items);
-
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    items: items,
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -46,6 +48,16 @@ app.post("/compose", function (req, res) {
 
   items.push(item);
   res.redirect("/");
+});
+
+app.get("/posts/:post", function (req, res) {
+  const param = _.lowerCase(req.params.post);
+
+  items.forEach(function (item) {
+    if (_.lowerCase(item.title) === param) {
+      res.render("post", { obj: item });
+    }
+  });
 });
 
 app.listen(3000, function () {
