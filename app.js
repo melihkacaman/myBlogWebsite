8ecbbc4f834eefcce548b0f268fae62b0a53234c
Melih Kaçaman <mkacamann@gmail.com>
2020-05-18 12:35:06 +0300

    mongodb implements

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,7 +15,23 @@ const contactContent =
 
 const app = express();
 
-const items = [];
+mongoose.connect("mongodb://localhost:27017/myBlogSite", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "You have to give a title."],
+  },
+  content: {
+    required: [true, "You have to give a content for an item."],
+    type: String,
+  },
+});
+
+const Item = mongoose.model("Item", itemSchema);
 
 app.set("view engine", "ejs");
 
@@ -22,9 +39,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    items: items,
+  Item.find({}, function (err, items) {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      items: items,
+    });
   });
 });
 
@@ -41,22 +60,21 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const item = {
+  const item = new Item({
     title: req.body.item,
     content: req.body.content,
-  };
-
-  items.push(item);
-  res.redirect("/");
+  }).save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/posts/:post", function (req, res) {
-  const param = _.lowerCase(req.params.post);
+  const param = req.params.post;
 
-  items.forEach(function (item) {
-    if (_.lowerCase(item.title) === param) {
-      res.render("post", { obj: item });
-    }
+  Item.findOne({ _id: param }, function (err, one) {
+    res.render("post", { obj: one });
   });
 });
 
